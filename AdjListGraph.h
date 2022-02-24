9aa6451816025a8ac4e4d68bd3948d1b6f6be409
abouttt <qw4898@naver.com>
2022-02-24 19:39:06 +0900

    modified member variable name, reserve()

--- AdjListGraph.h
+++ AdjListGraph.h
@@ -60,11 +60,11 @@ public:
 	explicit AdjListGraph() = default;
 
 	AdjListGraph(const AdjListGraph& other)
-		: mAdjList(other.mAdjList)
+		: mVertices(other.mVertices)
 	{}
 
 	AdjListGraph(AdjListGraph&& other) noexcept
-		: mAdjList(std::move(other.mAdjList))
+		: mVertices(std::move(other.mVertices))
 	{}
 
 	//
@@ -79,13 +79,13 @@ public:
 
 	AdjListGraph& operator=(const AdjListGraph& other)
 	{
-		mAdjList = other.mAdjList;
+		mVertices = other.mVertices;
 		return *this;
 	}
 
 	AdjListGraph& operator=(AdjListGraph&& other) noexcept
 	{
-		mAdjList = std::move(other.mAdjList);
+		mVertices = std::move(other.mVertices);
 		return *this;
 	}
 
@@ -95,22 +95,22 @@ public:
 
 	inline T& at(size_type pos)
 	{
-		return mAdjList.at(pos).mData;
+		return mVertices.at(pos).mData;
 	}
 
 	inline const T& at(size_type pos) const
 	{
-		return mAdjList.at(pos).mData;
+		return mVertices.at(pos).mData;
 	}
 
 	T& operator[](size_type pos)
 	{
-		return mAdjList[pos].mData;
+		return mVertices[pos].mData;
 	}
 
 	const T& operator[](size_type pos) const
 	{
-		return mAdjList[pos].mData;
+		return mVertices[pos].mData;
 	}
 
 	//
@@ -119,27 +119,27 @@ public:
 
 	inline bool empty() const noexcept
 	{
-		return mAdjList.empty();
+		return mVertices.empty();
 	}
 
 	inline size_type size() const noexcept
 	{
-		return mAdjList.size();
-	}
-
-	inline size_type max_size() const noexcept
-	{
-		return mAdjList.max_size();
+		return mVertices.size();
 	}
 
 	inline void reserve(size_type newCapacity)
 	{
-		mAdjList.reserve(newCapacity);
+		if (newCapacity <= mVertices.capacity())
+		{
+			return;
+		}
+
+		mVertices.reserve(newCapacity);
 	}
 
 	inline size_type capacity() const noexcept
 	{
-		return mAdjList.capacity();
+		return mVertices.capacity();
 	}
 
 	//
@@ -148,12 +148,12 @@ public:
 
 	inline void clear() noexcept
 	{
-		mAdjList.clear();
+		mVertices.clear();
 	}
 
 	inline void clear_edges()
 	{
-		for (auto& vertex : mAdjList)
+		for (auto& vertex : mVertices)
 		{
 			vertex.mEdges.clear();
 		}
@@ -162,44 +162,44 @@ public:
 	inline void clear_edges(const T& value)
 	{
 		size_type valueIndex = findIndex(value);
-		mAdjList[valueIndex].mEdges.clear();
+		mVertices[valueIndex].mEdges.clear();
 	}
 
 	// 정점 삭제시 해당 정점을 연결한 간선도 함께 삭제되나
 	// 값들이 앞당겨져 값이 상이할 수 있음. 간선들의 값 재조정 필요.
 	inline void erase_vertex(const T& value)
 	{
-		if (mAdjList.size() <= 0)
+		if (mVertices.size() <= 0)
 		{
 			return;
 		}
 
-		auto eraseIt = std::find(mAdjList.begin(), mAdjList.end(), value);
-		if (eraseIt == mAdjList.end())
+		auto eraseIt = std::find(mVertices.begin(), mVertices.end(), value);
+		if (eraseIt == mVertices.end())
 		{
 			return;
 		}
 
-		for (size_type i = 0; i < mAdjList.size(); i++)
+		for (size_type i = 0; i < mVertices.size(); i++)
 		{
-			if (mAdjList[i].mData == (*eraseIt))
+			if (mVertices[i].mData == (*eraseIt))
 			{
 				continue;
 			}
 
-			auto it = std::find(mAdjList[i].mEdges.begin(), mAdjList[i].mEdges.end(), &(*eraseIt));
-			if (it != mAdjList[i].mEdges.end())
+			auto it = std::find(mVertices[i].mEdges.begin(), mVertices[i].mEdges.end(), &(*eraseIt));
+			if (it != mVertices[i].mEdges.end())
 			{
-				mAdjList[i].mEdges.erase(it);
+				mVertices[i].mEdges.erase(it);
 			}
 		}
 
-		mAdjList.erase(eraseIt);
+		mVertices.erase(eraseIt);
 	}
 
 	inline void erase_edge(const T& from, const T& to)
 	{
-		if (mAdjList.size() <= 0)
+		if (mVertices.size() <= 0)
 		{
 			return;
 		}
@@ -207,36 +207,36 @@ public:
 		size_type fromIndex = findIndex(from);
 		size_type toIndex = findIndex(to);
 
-		auto eraseIt = std::find(mAdjList[fromIndex].mEdges.begin(), mAdjList[fromIndex].mEdges.end(), &mAdjList[toIndex]);
+		auto eraseIt = std::find(mVertices[fromIndex].mEdges.begin(), mVertices[fromIndex].mEdges.end(), &mVertices[toIndex]);
 
-		if (eraseIt == mAdjList[fromIndex].mEdges.end())
+		if (eraseIt == mVertices[fromIndex].mEdges.end())
 		{
 			return;
 		}
 
-		mAdjList[fromIndex].mEdges.erase(eraseIt);
+		mVertices[fromIndex].mEdges.erase(eraseIt);
 	}
 
 	inline void push_back(const T& value)
 	{
-		auto it = std::find(mAdjList.begin(), mAdjList.end(), value);
-		if (it != mAdjList.end())
+		auto it = std::find(mVertices.begin(), mVertices.end(), value);
+		if (it != mVertices.end())
 		{
 			return;
 		}
 
-		mAdjList.push_back(Vertex(value));
+		mVertices.push_back(Vertex(value));
 	}
 
 	inline void push_back(T&& value)
 	{
-		auto it = std::find(mAdjList.begin(), mAdjList.end(), value);
-		if (it != mAdjList.end())
+		auto it = std::find(mVertices.begin(), mVertices.end(), value);
+		if (it != mVertices.end())
 		{
 			return;
 		}
 
-		mAdjList.push_back(std::move(Vertex(value)));
+		mVertices.push_back(std::move(Vertex(value)));
 	}
 
 	inline void link_vertex(const T& from, const T& to)
@@ -249,7 +249,7 @@ public:
 			return;
 		}
 
-		mAdjList[fromIndex].mEdges.push_back(&mAdjList[toIndex]);
+		mVertices[fromIndex].mEdges.push_back(&mVertices[toIndex]);
 	}
 
 	inline void unlink_vertex(const T& from, const T& to)
@@ -262,13 +262,13 @@ public:
 			return;
 		}
 
-		auto eraseIt = std::find(mAdjList[fromIndex].mEdges.begin(), mAdjList[fromIndex].mEdges.end(), &mAdjList[toIndex]);
-		if (eraseIt == mAdjList[fromIndex].mEdges.end())
+		auto eraseIt = std::find(mVertices[fromIndex].mEdges.begin(), mVertices[fromIndex].mEdges.end(), &mVertices[toIndex]);
+		if (eraseIt == mVertices[fromIndex].mEdges.end())
 		{
 			return;
 		}
 
-		mAdjList[fromIndex].mEdges.erase(eraseIt);
+		mVertices[fromIndex].mEdges.erase(eraseIt);
 	}
 
 	inline bool is_linked(const T& from, const T& to)
@@ -281,8 +281,8 @@ public:
 			return false;
 		}
 
-		auto eraseIt = std::find(mAdjList[fromIndex].mEdges.begin(), mAdjList[fromIndex].mEdges.end(), &mAdjList[toIndex]);
-		if (eraseIt != mAdjList[fromIndex].mEdges.end())
+		auto eraseIt = std::find(mVertices[fromIndex].mEdges.begin(), mVertices[fromIndex].mEdges.end(), &mVertices[toIndex]);
+		if (eraseIt != mVertices[fromIndex].mEdges.end())
 		{
 			return true;
 		}
@@ -292,18 +292,18 @@ public:
 
 	inline void swap(AdjListGraph& other) noexcept
 	{
-		std::swap(mAdjList, other.mAdjList);
+		std::swap(mVertices, other.mVertices);
 	}
 
 	inline void dfs_search(const T& start)
 	{
-		std::vector<bool> visited(mAdjList.size(), false);
+		std::vector<bool> visited(mVertices.size(), false);
 		dfs(start, visited);
 	}
 
 	inline void bfs_search(const T& start)
 	{
-		std::vector<bool> discovered(mAdjList.size(), false);
+		std::vector<bool> discovered(mVertices.size(), false);
 		std::queue<size_type> q;
 		size_type startIndex = findIndex(start);
 		q.push(startIndex);
@@ -316,9 +316,9 @@ public:
 			currentInex = q.front();
 			q.pop();
 
-			std::cout << mAdjList[currentInex].mData << std::endl;
+			std::cout << mVertices[currentInex].mData << std::endl;
 
-			for (auto& next : mAdjList[currentInex].mEdges)
+			for (auto& next : mVertices[currentInex].mEdges)
 			{
 				nextIndex = findIndex(next->mData);
 				if (discovered[nextIndex])
@@ -335,9 +335,9 @@ public:
 private:
 	inline size_type findIndex(const T& value)
 	{
-		for (size_type i = 0; i < mAdjList.size(); i++)
+		for (size_type i = 0; i < mVertices.size(); i++)
 		{
-			if (mAdjList[i].mData == value)
+			if (mVertices[i].mData == value)
 			{
 				return i;
 			}
@@ -353,18 +353,17 @@ private:
 
 		std::cout << current << std::endl;
 
-		for (size_type i = 0; i < mAdjList[currentIndex].mEdges.size(); i++)
+		for (size_type i = 0; i < mVertices[currentIndex].mEdges.size(); i++)
 		{
-			T next = mAdjList[currentIndex].mEdges[i]->mData;
+			T next = mVertices[currentIndex].mEdges[i]->mData;
 			size_type nextIndex = findIndex(next);
 			if (visited[nextIndex] == false)
 			{
-				dfs(mAdjList[nextIndex].mData, visited);
+				dfs(mVertices[nextIndex].mData, visited);
 			}
 		}
 	}
 
 private:
-	std::vector<Vertex> mAdjList;
+	std::vector<Vertex> mVertices;
 };
-
